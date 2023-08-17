62a20d2c8573c820dbc429251d06b8b9e2a7a71f
Marcu Operario <marcuoperario@gmail.com>
2023-08-18 07:02:41 +0800

    feat: add Scrolldown section

---
+++ components/hero/scrolldown.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FaAngleDoubleDown } from "react-icons/fa";
+import { navlinks } from '@/constants';
+import { LuMouse } from "react-icons/lu";
+
+const Scrolldown = () => {
+  return (
+    <div className = "flex flex-col items-center justify-center w-full gap-y-6 animate-pulse duration-500">
+      <div className = "flex flex-row items-center gap-2 text-xl text-white" >
+        <LuMouse />
+        <span>
+          Scroll down
+        </span>
+      </div>
+      <a href = { navlinks[1].path }>
+        <FaAngleDoubleDown className = "text-3xl animate-bounce duration-1000"/>
+      </a>
+    </div>
+  )
+};
+
+export default Scrolldown;
