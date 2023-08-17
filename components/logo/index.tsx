662d18aa68cbcc218ce4e11673ac70fb6dced021
Marcu Operario <marcuoperario@gmail.com>
2023-08-18 07:03:53 +0800

    feat: add Logo component

---
+++ components/logo/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FaFishFins } from "react-icons/fa6";
+
+const Logo = () => {
+  return (
+    <FaFishFins className = "text-4xl"/>
+  )
+};
+
+export default Logo;
