ed9279d0741485f73619dc9bd6e20f9061bbb05f
Marcu Operario <marcuoperario@gmail.com>
2023-08-18 07:13:21 +0800

    feat: add alias exports for public

---
+++ public/index.ts
@@ -0,0 +1,48 @@
+export { default as NextLogo } from "./next.svg";
+export { default as VercelLogo } from "./vercel.svg";
+export { default as TailwindCSSLogo } from "./tailwindcss-logotype.svg";
+export { default as FramerMotionLogo } from "./framer.svg";
+export { default as HeroIllustration } from "./web-template.png";
+export { default as GraduationPhoto } from "./grad.jpg";
+
+export { default as DurTectThumbnail } from "./durtect/durtect.svg";
+export { default as DurTectMockup } from "./durtect/durtect-mockup.png";
+
+export { default as MaskUpThumbnail } from "./maskup/maskup.svg";
+export { default as MaskUpUser } from "./maskup/maskup-user.png";
+export { default as MaskUpAdmin } from "./maskup/maskup-admin.png";
+
+export { default as BugritosThumbnail } from "./bugritos/bugritos.svg";
+export { default as BugritosDashboard } from "./bugritos/bugritos-dashboard.png";
+export { default as BugritosEmployees } from "./bugritos/bugritos-employees.png";
+export { default as BugritosProducts } from "./bugritos/bugritos-products.png";
+export { default as BugritosProfile } from "./bugritos/bugritos-profile.png";
+export { default as BugritosSignin } from "./bugritos/bugritos-signin.png";
+export { default as BugritosSignup } from "./bugritos/bugritos-signup.png";
+export { default as BugritosTransactions } from "./bugritos/bugritos-transactions.png";
+
+export { default as AnimeLibThumbnail } from "./animelib/animelib.svg";
+export { default as AnimeLibDashboard } from "./animelib/animelib-dashboard.png";
+export { default as AnimeLibForgotPassword } from "./animelib/animelib-forgotpassword.png";
+export { default as AnimeLibInformation } from "./animelib/animelib-information.png";
+export { default as AnimeLibProfile } from "./animelib/animelib-profile.png";
+export { default as AnimeLibSignin } from "./animelib/animelib-signup.png";
+export { default as AnimeLibSignup } from "./animelib/animelib-signup.png";
+export { default as AnimeLibWatchlist } from "./animelib/animelib-watchlist.png";
+
+export { default as ShoppingListThumbnail } from "./shoppinglist/shoppinglist.svg";
+export { default as ShoppingListCreate } from "./shoppinglist/shoppinglist-create.png";
+export { default as ShoppingListHelp } from "./shoppinglist/shoppinglist-help.png";
+export { default as ShoppingListView } from "./shoppinglist/shoppinglist-view.png";
+export { default as ShoppingListViewAndDelete } from "./shoppinglist/shoppinglist-view&delete.png";
+
+export { default as ShortlyThumbnail } from "./shortly/shortly.svg";
+export { default as ShortlyHome } from "./shortly/shortly-home.png";
+
+export { default as BookHiveThumbnail } from "./bookhive/bookhive.svg";
+export { default as BookHiveHome } from "./bookhive/bookhive-home.png";
+export { default as BookHiveAdd } from "./bookhive/bookhive-add.png";
+export { default as BookHiveUpdate } from "./bookhive/bookhive-update.png";
+
+export { default as ListifyThumbnail } from "./listify/listify.svg";
+export { default as ListifyHome } from "./listify/listify-home.png";
