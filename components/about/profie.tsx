b1e29a876324e66f816c428d9e73365875d2def0
Marcu Operario <marcuoperario@gmail.com>
2023-08-18 06:59:05 +0800

    feat: add AboutMe section

---
+++ components/about/profie.tsx
@@ -0,0 +1,76 @@
+import { 
+    socmedlinks,
+    techstack
+} from '@/constants';
+import React from 'react';
+import Image from 'next/image';
+import { GraduationPhoto } from '@/public';
+
+const AboutMe = () => {
+    return (
+        <div className = "space-y-10">
+            <h2 className = "text-3xl">
+                About Me
+            </h2>
+            <div className = "flex lg:flex-row flex-col item-center w-full gap-10">
+                <Image 
+                    src = { GraduationPhoto }
+                    alt = ""
+                    className = "lg:w-80 w-full rounded-lg"
+                    priority
+                />
+                <div className = "w-full flex flex-col justify-between items-start space-y-4">
+                    <p className = "text-xl text-justify ">
+                        My name is Marcu Pelayo L. Operario. I am an aspiring Software Engineer who is committed to expanding my expertise in software development concepts and methodologies. I recently graduated with a Bachelor's Degree in Computer Science from Mapúa Malayan Colleges Mindanao. I am excited to join a team and contribute to developing high-quality software solutions.
+                    </p>
+                    <p className = "text-xl">
+                        Here are some technologies that I have used in various projects:
+                    </p>
+                    <ul className = "grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full">
+                        { techstack.map((val, i) => (
+                            <li 
+                                key = { i }
+                                className = "flex flex-row items-center gap-3"
+                            >
+                                <span className = "text-2xl">
+                                    { val.icon }
+                                </span>
+                                <span className = "text-md">
+                                    { val.label }
+                                </span>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className = "w-full flex items-center sm:flex-row flex-col gap-4">
+                        <a
+                            href = "/resume.pdf"
+                            target = "_blank"
+                            className = "w-full border border-[#2C3031] text-white rounded-lg hover:bg-white hover:text-black duration-300 p-4 font-bold flex justify-center items-center"
+                        >
+                            Resume
+                        </a>
+                        { socmedlinks.map((val, i) => (
+                            <div 
+                                key = { i }
+                                className = "w-full lg:w-auto"
+                            >
+                                <a
+                                    href = { val.path }
+                                    target = "_blank"
+                                    className = "w-full border border-[#2C3031] rounded-xl p-4 flex justify-center items-center space-x-2 hover:bg-white hover:text-black duration-300 text-xl cursor-pointer"
+                                >
+                                    { val.icon }
+                                    <span className = "sm:hidden">
+                                        { val.label }
+                                    </span>
+                                </a>
+                            </div> 
+                        ))}
+                    </div> 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutMe;
