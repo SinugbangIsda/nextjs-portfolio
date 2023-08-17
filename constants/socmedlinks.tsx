bb67ff8077539bb695bf1151db24b71ed49c4bf9
Marcu Operario <marcuoperario@gmail.com>
2023-08-18 07:12:17 +0800

    feat: add SocMedLinks constant

---
+++ constants/socmedlinks.tsx
@@ -0,0 +1,29 @@
+import { 
+    FaFacebook,
+    FaLinkedinIn,
+    FaInstagram,
+    FaGithub
+} from "react-icons/fa";
+
+export const socmedlinks = [
+    {
+        label: "Facebook",
+        path: "https://www.facebook.com/mpoperario/",
+        icon: <FaFacebook />
+    },
+    {
+        label: "Linkedin",
+        path: "https://www.linkedin.com/in/marcuoperario/",
+        icon: <FaLinkedinIn />
+    },
+    {
+        label: "Github",
+        path: "https://github.com/SinugbangIsda/",
+        icon: <FaGithub />
+    },
+    {
+        label: "Instagram",
+        path: "https://www.instagram.com/marcuoperario/",
+        icon: <FaInstagram />
+    }
+];
