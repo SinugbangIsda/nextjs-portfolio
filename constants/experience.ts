0bbae5e21cebde464c0aae11c67ca8ef82c2d652
Marcu Operario <marcuoperario@gmail.com>
2023-08-18 07:11:34 +0800

    feat: add experience constant

---
+++ constants/experience.ts
@@ -0,0 +1,31 @@
+export const experience = [
+    {
+        heading: "ChatChatABC Philippines Inc. - Web dev Intern",
+        company: "ChatChatABC Philippines Inc.",
+        date: "March 2023 - May 2023",
+        tools: [
+            "Java",
+            "Spring Boot",
+            "ReactJS"
+        ],
+        responsibilities: [
+            "Explored the Spring Boot framework and leveraged its various tools and features to develop multiple web applications, primarily using ReactJS.",
+            "Created slide presentations using Slidev for my web projects, effectively showcasing insights gleaned from my research on development tools and practices.",
+            "Deployed web applications using Cloudfare Pages."
+        ]
+    },
+    {
+        heading: "HighDef Supply & Services Corp. - Executive Office Assistant",
+        company: "HighDef Supply & Services Corp.",
+        date: "May 2022 - August 2022",
+        tools: [
+            "MS Excel",
+            "MS Word"
+        ],
+        responsibilities: [
+            "Meticulously monitored daily project expenses utilizing Microsoft Excel and Microsoft Word.",
+            "Managed the executive's daily tasks and schedule, while also creating and organizing project documents.",
+            "Developed and oversaw spreadsheets for data tracking and analysis."
+        ]
+    }
+];
