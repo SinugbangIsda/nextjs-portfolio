47edccc721dda574f51b276ce838ec8c25040ee9
Marcu Operario <marcuoperario@gmail.com>
2023-08-22 03:55:35 +0800

    refactor: add responsive font

--- components/pill/index.tsx
+++ components/pill/index.tsx
@@ -5,10 +5,10 @@ interface PillProps {
 }
 
 const Pill = ({ 
-    children
+  children
 }: PillProps) => {
   return (
-    <div className = "bg-[#191A1D] rounded-full py-2 px-4 text-sm font-bold border border-white flex justify-center items-center text-center">
+    <div className = "bg-[#191A1D] rounded-full py-2 px-4 text-xs sm:text-sm font-bold border border-white flex justify-center items-center text-center">
       { children }
     </div>
   )
